30338a4115d4084868da2da60f0031be29a57f4d
Marien Wolthuis <marien@orion-am.com>
2026-08-11 19:13:21 +0200

    Bonus lesson: restructure content into content collection for efficiency?

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,17 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
 
 export async function GET(context) {
+    const posts = await getCollection("blog");
   return rss({
     title: 'Astro Learners testing | Blog amateurism',
     description: 'Just following a tutorial',
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
-    customData: `<language>en-us</language>`,
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/posts/${post.id}/`,
+    })),    customData: `<language>en-us</language>`,
   });
 }
